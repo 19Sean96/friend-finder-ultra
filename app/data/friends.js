3fa3e462617258ea44a278945ea18d64ab9e1986
Sean A <1996.sean.alexander@gmail.com>
2019-08-20 14:28:57 -0700

    completed friend matching logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,7 @@ let friends = [
     {
         name: "Bob",
         photo:
-            "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkghBxQyqLV8IeLa0eKD2P5weJusLFAiO-_2E9p-tSAgC4NPfF",
         scores: [1, 2, 3, 2, 4, 5, 1, 1, 3, 2]
     },
     {
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,11 @@ const friendData = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/api/friends", (req,res) => {
-        console.log(res);
         res.json(friendData);
+    });
+
+    app.post("/api/friends", (req,res) => {
+        console.log(req);
     });
 
 }
